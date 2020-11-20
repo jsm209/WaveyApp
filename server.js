6b39689fec44f9d99f9091ef6a89cfa83322dd87
Joshua Maza <joshuasadirimaza@gmail.com>
2020-11-19 19:30:40 -0800

    attempt to load html file as response

--- server.js
+++ server.js
@@ -2,23 +2,28 @@ const http = require('http');
 const express = require("express");
 
 const fs = require('fs');
+const { response } = require('express');
 
 const app = express();
 
 // Getting our resources
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
-
-app.get('*', (req, res) => {
-    res.sendFile('/public/index.html');
-})
+var htmlFile;
+fs.readFile('./public.html', function(err, data) {
+    if (err) {
+        throw err;
+    }
+    htmlFile = data;
+});
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.write('Node.js says hello!'); //write a response to the client
-    res.end(); //end the response
+
+    // Serves different files based on url
+    switch(req.url) {
+        case "/index.html" :
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(htmlFile);
+            break;
+    }
 });
 
   server.listen(process.env.PORT || 3000) // Specifies the port to whatever heroku gives us or 5000 on localhost.
